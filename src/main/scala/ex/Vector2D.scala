61062a79074de6623733386751af2121de833fcf
Tommaso <tommaso.brini@studio.unibo.it>
2025-03-31 10:54:37 +0200

    solved ex1

--- src/main/scala/ex/Vector2D.scala
+++ src/main/scala/ex/Vector2D.scala
@@ -25,7 +25,14 @@ trait Vector2D:
 
 object Vector2D:
   // Factory method to create Vector2D instances
-  def apply(x: Double, y: Double): Vector2D = ???
+  def apply(x: Double, y: Double): Vector2D = Vector2DImpl(x,y)
+
+  private case class Vector2DImpl(override val x: Double, override val y: Double) extends Vector2D:
+    override def +(other: Vector2D): Vector2D = Vector2DImpl(x + other.x, y + other.y)
+    override def -(other: Vector2D): Vector2D = Vector2DImpl(x - other.x, y - other.y)
+    override def *(scalar: Double): Vector2D = Vector2DImpl(x * scalar, y * scalar)
+    override def dot(other: Vector2D): Double = x * other.x + y * other.y
+    override def magnitude: Double = sqrt(x*x + y*y)
 
   // Common vectors (optional but nice)
   val zero: Vector2D = apply(0.0, 0.0)
